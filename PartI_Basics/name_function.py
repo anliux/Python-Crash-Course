9817ba7da916d187a3f08e5a9538082b8fc13e52
anliux <luckyxuan_1123@163.com>
2019-03-13 20:35:28 +0800

    update 11 on 0313

--- PartI_Basics/name_function.py
+++ PartI_Basics/name_function.py
@@ -1,3 +1,8 @@
-def get_formatted_name(first, last):
-    full_name = first + ' ' + last
+# name_function.py 最终的样子
+def get_formatted_name(first, last, middle=''):
+    # 改为可妥善处理中间名的，即当没有中间名时也不会报错
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last 
     return full_name.title()
